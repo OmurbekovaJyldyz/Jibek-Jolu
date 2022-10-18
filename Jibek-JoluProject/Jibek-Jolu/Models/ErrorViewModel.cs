c834a5ef3fe2d675ae6099c44b915577f8d7a568
IslamMoldobekov <islam.moldobekov02@gmail.com>
2022-10-18 10:47:59 +0600

    Added Auth

---
+++ Jibek-JoluProject/Jibek-Jolu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jibek_Jolu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
